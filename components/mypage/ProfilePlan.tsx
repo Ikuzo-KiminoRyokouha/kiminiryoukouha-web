b1da6e72b462bf30dcec46fc9992b106b8592a5a
Defiler1 <6430370@naver.com>
2023-03-09 15:24:16 +0900

    🐛 fix : Profile page bug fixed

--- components/mypage/ProfilePlan.tsx
+++ components/mypage/ProfilePlan.tsx
@@ -9,9 +9,16 @@ export default function ProfilePlan({ planInfo }) {
         {test()}
       </div>
       <div className="h-10 w-full"></div> */}
+      {planInfo?.data?.message === "not found any plan" && (
+        <div className="flex min-h-[8rem] items-center justify-center pt-16">
+          <span className="text-2xl font-semibold">
+            There isn't any Plans yet
+          </span>
+        </div>
+      )}
       <div className="grid  grid-cols-1 gap-y-10 pt-10 pl-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {planInfo ? (
-          planInfo?.data?.plans.map((el) => {
+          planInfo?.data?.plans?.map((el) => {
             // console.log("el123", el);
             return (
               <DecidedPlanCard
